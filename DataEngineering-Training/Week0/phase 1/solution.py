c923f6e6d1e928583c2939db2b96d0e9d6e1fcf2
GANESH GUDALA <ganeshgudala2003@gmail.com>
2026-04-03 18:57:21 +0530

    Update solution.py

--- DataEngineering-Training/Week0/phase 1/solution.py
+++ DataEngineering-Training/Week0/phase 1/solution.py
@@ -2,34 +2,23 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Phase1").getOrCreate()
 
-#Create DataFrame (same as SQL table data)
 customers = spark.createDataFrame([
     (1, "Ravi", "Hyderabad", 25),
     (2, "Sita", "Chennai", 32),
     (3, "Arun", "Hyderabad", 28)
 ], ["customer_id", "customer_name", "city", "age"])
 
-
-SELECT * FROM customers;
-print("1. Show all customers")
+print("All Customers")
 customers.show()
 
-
-SELECT * FROM customers WHERE city = 'Chennai';
-print("2. Customers from Chennai")
+print("Customers from Chennai")
 customers.filter(customers.city == "Chennai").show()
 
-
-SELECT * FROM customers WHERE age > 25;
-print("3. Customers with age > 25")
+print("Customers age > 25")
 customers.filter(customers.age > 25).show()
 
-
-SELECT customer_name, city FROM customers;
-print("4. Select name and city")
+print("Selected Columns")
 customers.select("customer_name", "city").show()
 
-
-SELECT city, COUNT(*) FROM customers GROUP BY city;
-print("5. Count customers city-wise")
+print("City-wise Count")
 customers.groupBy("city").count().show()
