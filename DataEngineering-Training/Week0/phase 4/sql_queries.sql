49b1e5321b4d16ab728ccf9e6048ff08cac6c887
GANESH GUDALA <ganeshgudala2003@gmail.com>
2026-04-05 11:14:44 +0530

    Update sql_queries.sql

--- DataEngineering-Training/Week0/phase 4/sql_queries.sql
+++ DataEngineering-Training/Week0/phase 4/sql_queries.sql
@@ -1,4 +1,38 @@
--- Daily Sales
+-- Create Customers Table
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+    customer_id INTEGER,
+    customer_name TEXT,
+    city TEXT,
+    age INTEGER
+);
+
+INSERT INTO customers VALUES
+(1, 'Ravi', 'Hyderabad', 25),
+(2, 'Meena', 'Chennai', 30),
+(3, 'John', 'Bangalore', 28),
+(4, 'Arun', 'Hyderabad', 35),
+(5, 'Kiran', 'Chennai', 40);
+
+-- Create Orders Table
+DROP TABLE IF EXISTS orders;-- Daily Sales
+CREATE TABLE orders (
+    order_id INTEGER,
+    customer_id INTEGER,
+    amount INTEGER,
+    order_date TEXT
+);
+
+INSERT INTO orders VALUES
+(101, 1, 5000, '2024-01-01'),
+(102, 1, 7000, '2024-01-02'),
+(103, 2, 3000, '2024-01-01'),
+(104, 3, 8000, '2024-01-03'),
+(105, 3, 2000, '2024-01-03'),
+(106, 4, 15000, '2024-01-04'),
+(107, 5, 1000, '2024-01-02');
+
 SELECT order_date, SUM(amount)
 FROM orders
 GROUP BY order_date;
